4032822cf29ae632b859124d9cf34194a6191d18
hrdk224 <hardikkille07@gmail.com>
2026-05-26 17:55:27 +0530

    add nullish

---
+++ day01-javascript-core/2.12 Nullish coalescing operator ??/nullish.js
@@ -0,0 +1,5 @@
+// The nullish coalescing operator (??) is a short-circuiting operator that returns its
+// right-hand operand only when its left-hand operand is strictly null or undefined.
+
+// set height=100, if height is null or undefined
+height = height ?? 100;
