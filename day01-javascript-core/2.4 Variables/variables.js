49a9e6f60d4504ccb1f432ed1a57ca02d7b63ca7
hrdk224 <hardikkille07@gmail.com>
2026-05-26 11:32:46 +0530

    add variables

---
+++ day01-javascript-core/2.4 Variables/variables.js
@@ -0,0 +1,8 @@
+// Variables can be declred using let, var(old convention), const(cannot be changed once declared)
+
+let message = 'hello World!';
+message = 'Hi';
+console.log(message);
+
+const COLOR = 'Red';
+console.log(COLOR);
